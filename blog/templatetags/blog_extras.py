b4d694aa616802182e2032f772d7a1d170df5efd
Allan Stocco <allanstoccorezende@gmail.com>
2022-05-09 18:28:04 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import escape, format_html
 from django.utils.safestring import mark_safe
+from blog.models import *
 
 
 
@@ -30,3 +31,30 @@ def author_details(author, current_user):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+# row tags
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+# col tags
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import *
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
@@ -10,3 +11,7 @@ def index(request):
     return render(request, "blog/index.html", {
       "posts": posts
       })
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post": post})
