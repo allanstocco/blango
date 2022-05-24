5a494332e93a0d80b4111c7a7cb215bba7b23d53
Allan Stocco <allanstoccorezende@gmail.com>
2022-05-24 13:37:46 +0000

    Finish Django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -58,12 +58,18 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      
       'django.contrib.staticfiles',
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
       'debug_toolbar',
-      'blango_auth'
+      'blango_auth',
+      'django.contrib.sites',
+      'allauth',
+      'allauth.account',
+      'allauth.socialaccount',
+      'allauth.socialaccount.providers.google',
   ]
 
   MIDDLEWARE = [
@@ -79,6 +85,12 @@ class Dev(Configuration):
   ]
 
   ROOT_URLCONF = 'blango.urls'
+
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
 
   AUTH_USER_MODEL = "blango_auth.User"
 
--- blango/urls.py
+++ blango/urls.py
@@ -27,6 +27,7 @@ from blango_auth.forms import BlangoRegistrationForm
 urlpatterns = [
 
     path("accounts/", include("django.contrib.auth.urls")),
+    
 
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
     path("accounts/register/", RegistrationView.as_view(form_class=BlangoRegistrationForm), name="django_registration_register"),
@@ -42,6 +43,8 @@ urlpatterns = [
     name="django_registration_register",
 ),
     path("accounts/", include("django_registration.backends.activation.urls")),
+    path("accounts/", include("allauth.urls")),
+    
 ]
 
 if settings.DEBUG:
